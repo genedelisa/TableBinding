7d725e538d0b1d1e3e80b87324d35a7f471b35dc
Gene De Lisa <gene@rockhoppertech.com>
2015-07-10 17:01:08 -0400

    Initial Commit

---
+++ TableBinding/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableBinding
+//
+//  Created by Gene De Lisa on 7/10/15.
+//  Copyright © 2015 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TableBinding/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableBinding
+//
+//  Created by Gene De Lisa on 7/10/15.
+//  Copyright © 2015 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
